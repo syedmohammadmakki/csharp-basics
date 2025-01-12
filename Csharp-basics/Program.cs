22d1084a4dde806218cd851bd51db772c3e31655
Mohammad Makki <mohammadmakkisyed@gmail.com>
2025-01-11 20:23:39 -0500

    initial Csharp solution

---
+++ Csharp-basics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csharp_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
