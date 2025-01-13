7df45ef6c218bb4f18b8bd1d5d21fc690160ecdd
Mohammad Makki <mohammadmakkisyed@gmail.com>
2025-01-13 18:34:00 -0500

    exicersice one complete

--- Csharp-basics/Program.cs
+++ Csharp-basics/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, simba how are you");
+            Console.Write("inter a nuber :");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+           if (number <= 10)
+            {
+                Console.WriteLine("valed");
+
+            }else if (number >11) 
+            {
+                Console.WriteLine("invalid");
+            }else
+            {
+                Console.WriteLine("please inter  a nuber ");
+            }
         }
     }
 }
