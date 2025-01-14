a819a4e59fded2b92b92b69f270c30f253369299
Mohammad Makki <mohammadmakkisyed@gmail.com>
2025-01-14 15:20:01 -0500

    second exercises

--- Csharp-basics/Program.cs
+++ Csharp-basics/Program.cs
@@ -1,16 +1,25 @@
-﻿namespace Csharp_basics
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System;
+
+namespace Csharp_basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("inter a nuber :");
-            var number = Convert.ToInt32(Console.ReadLine());
 
-           if (number <= 10)
+            //Exercises-1
+           // 1 - Write a program and ask the user to enter a number.
+           // The number should be between 1 to 10.If the user enters a valid number,
+           // display "Valid" on the console.Otherwise, display "Invalid".
+           // (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+
+            Console.Write("inter a number :");
+            var number = Convert.ToInt32(Console.ReadLine());
+            if (number <= 10)
             {
                 Console.WriteLine("valed");
-
             }else if (number >11) 
             {
                 Console.WriteLine("invalid");
---
+++ Exercises-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace Exercises_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  2 - Write a program which takes two numbers
+            //  from the console and displays the maximum of the two.
+
+            Console.Write("inter a number first number :");
+            var numberOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("inter a second number :");
+            var secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (numberOne > secondNumber)
+            {
+                Console.WriteLine(numberOne);
+            }else if (secondNumber > numberOne)
+            {
+                Console.WriteLine(secondNumber);
+            }
+
+        }
+    }
+}
